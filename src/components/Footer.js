aa1413949f0e5e5751969c7575eb970bc1dbb23a
Denzel Granadoz <zelgranadoz@gmail.com>
2022-08-12 21:29:36 +0800

    added & finished styling footer

---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer class="footer">
+      <div class="footer-contents">
+        <span>
+          <a href="https://github.com/DenzelGranadoz" target="_blank">
+            Made by: Denzel Granadoz
+          </a>
+        </span>
+        <span>:</span>
+        <span>
+          <a
+            href="https://github.com/DenzelGranadoz/memory-card"
+            target="_blank"
+          >
+            Click me for Source Code
+          </a>
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
